eaa203b5e35b205d817e14589b435b71eff42231
KAVITA MEENA <97969808+kavitameena108@users.noreply.github.com>
2024-10-19 12:19:33 +0530

    Update RatingAndReview.js

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -1,5 +1,5 @@
-const RatingAndReview = require("../models/RatingandReview")
-const Course = require("../models/Course")
+const RatingAndReview = require("../models/RatingAndReview")
+const Course = require("../models/Course.js")
 const mongoose = require("mongoose")
 
 // Create a new rating and review
